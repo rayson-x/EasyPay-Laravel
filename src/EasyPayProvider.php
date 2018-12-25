c9fd257994bfe23255c2f92fcb7235a5d67e3d45
qt007 <xurh@qtdeMacBook-Pro.local>
2018-12-25 13:39:51 +0800

    优化代码，减少全局变量使用

--- src/EasyPayProvider.php
+++ src/EasyPayProvider.php
@@ -3,13 +3,15 @@
 namespace EasyPay\Laravel;
 
 use EasyPay\Config;
+use EasyPay\Notify;
 use EasyPay\PayFactory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 
 class EasyPayProvider extends ServiceProvider
 {
-    protected $trades = [
-        // 支付宝可用操作
+    protected $services = [
+        // alipay
         'ali.qr.pay',
         'ali.wap.pay',
         'ali.refund',
@@ -17,7 +19,7 @@ class EasyPayProvider extends ServiceProvider
         'ali.query.order',
         'ali.close.order',
         'ali.refund.query',
-        // 微信可用操作
+        // wechat
         'wechat.qr.pay',
         'wechat.pub.pay',
         'wechat.app.pay',
@@ -29,7 +31,7 @@ class EasyPayProvider extends ServiceProvider
         'wechat.refund.query',        
     ];
 
-    protected $notify = [
+    protected $notifies = [
         'wechat.notify',
         'ali.notify',
     ];
@@ -42,12 +44,47 @@ class EasyPayProvider extends ServiceProvider
     protected $defer = false;
 
     /**
-     * Bootstrap commands.
+     * Bootstrap services.
      *
      * @return void
      */
-    protected function registerArtisanCommands()
+    public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/config.php' => config_path('easyPay.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register services
+        foreach ($this->services as $service) {
+            $this->app->bind($service, function ($app) use ($service) {
+                $path    = Arr::first(explode('.', $service));
+                $options = config("easyPay.{$path}");
+
+                return PayFactory::create($service, $options);
+            });
+        }
+
+        foreach ($this->notifies as $notify) {
+            $this->app->bind($service, function ($app) use ($service) {
+                $path    = Arr::first(explode('.', $service));
+                $options = config("easyPay.{$path}");
+                $request = $app->get('request');
+
+                return Notify::fromRequest($service, $request, $options);
+            });
+        }
+
+        // Register commands
         $this->commands([
             Commands\AliPayCommand::class,
             Commands\WechatPayCommand::class,
@@ -61,46 +98,12 @@ class EasyPayProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__ . '/config.php' => config_path('easyPay.php'),
-        ], 'config');
-
-        $config = $this->app['config']['easyPay'];
-
-        if (is_null($config)) {
-            $config = [];
-        }
-
-        Config::loadConfig($config);
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        foreach ($this->trades as $service) {
-            $this->app->bind($service, function ($app) use ($service) {
-                return PayFactory::create($service);
-            });
-        }
-    }
-
-    /**
      * Get the services provided by the provider.
      *
      * @return array
      */
     public function provides()
     {
-        return array_merge($this->trades, $this->notify);
+        return array_merge($this->services, $this->notify);
     }
 }
--- src/config.php
+++ src/config.php
@@ -4,24 +4,24 @@ return [
     // 微信配置信息
     'wechat' => [
         // 应用id
-        'appid'         => '',
-        // 商户支付密钥
-        'key'           => '',
-        // 商户号
-        'mch_id'        => '',
-        // 签名加密方式
-        'sign_type'     => 'MD5',
-        // 异步通知地址
-        'notify_url'    => '',
+        'appid'             =>  env('WEIXIN_APPID'),
+        // 应用密钥
+        'key'               =>  env('WEIXIN_MCH_KEY'),
+        // 商户ID
+        'mch_id'            =>  env('WEIXIN_MCH_ID'),
+        // 回调地址
+        'notify_url'        =>  'http://example.com',
         // ssl证书路径
-        'ssl_cert_path' => '',
+        'ssl_cert_path'     =>  env('WEIXIN_CERT_FILE'),
         // ssl密钥路径
-        'ssl_key_path'  => '',
+        'ssl_key_path'      =>  env('WEIXIN_KEY_FILE'),
+        // 签名加密方式
+        'sign_type'         => 'MD5',
     ],
     // 支付宝配置信息
     'ali' => [
         // 应用id
-        'app_id'            =>  '',
+        'app_id'            =>  env('ALIPAY_APPID'),
         // 沙箱测试开关
         'is_sand_box'       =>  true,
         // 生成的RSA证书私钥,用于生成签名
@@ -33,7 +33,7 @@ return [
         // 字符编码
         'charset'           =>  'UTF-8',
         // 签名加密方式
-        'sign_type'         =>  'RSA',
+        'sign_type'         =>  'RSA2',
         // 支付宝api版本
         'version'           =>  '1.0',
         // 销售产品码
